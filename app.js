1360078f42c8df95cc071d433c024cb3b524e342
Benji <73948537+Fletcher-Benji@users.noreply.github.com>
2021-10-13 12:21:24 +0100

    Add files via upload
    //reupload Tarot cards

--- app.js
+++ app.js
@@ -1,117 +1,116 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //card options
-    const cardArray = [
-      {
-        name: 'cake',
-        img: 'assets/cake.png'
-      },
-      {
-        name: 'cookie',
-        img: 'assets/cookie.png'
-      },
-      {
-        name: 'fish',
-        img: 'assets/fish.png'
-      },
-      {
-        name: 'picaxe',
-        img: 'assets/picaxe.png'
-      },
-      {
-        name: 'pork',
-        img: 'assets/pork.png'
-      },
-      {
-        name: 'record',
-        img: 'assets/record.png'
-      },
-      {
-        name: 'cake',
-        img: 'assets/cake.png'
-      },
-      {
-        name: 'cookie',
-        img: 'assets/cookie.png'
-      },
-      {
-        name: 'fish',
-        img: 'assets/fish.png'
-      },
-      {
-        name: 'picaxe',
-        img: 'assets/picaxe.png'
-      },
-      {
-        name: 'pork',
-        img: 'assets/pork.png'
-      },
-      {
-        name: 'record',
-        img: 'assets/record.png'
-      }
-    ]
-  
-    cardArray.sort(() => 0.5 - Math.random())
-  
-    const grid = document.querySelector('.grid')
-    const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
-    let cardsWon = []
-  
-    //create your board
-    function createBoard() {
-      for (let i = 0; i < cardArray.length; i++) {
-        const card = document.createElement('img')
-        card.setAttribute('src', 'assets/blank.png')
-        card.setAttribute('data-id', i)
-        card.addEventListener('click', flipCard)
-        grid.appendChild(card)
-      }
+  //card options
+  const cardArray = [
+    {
+      name: 'chariot',
+      img: 'images/chariot.png'
+    },
+    {
+      name: 'emperor',
+      img: 'images/emperor.png'
+    },
+    {
+      name: 'fool',
+      img: 'images/fool.png'
+    },
+    {
+      name: 'hierophant',
+      img: 'images/hierophant.png'
+    },
+    {
+      name: 'lovers',
+      img: 'images/lovers.png'
+    },
+    {
+      name: 'magician',
+      img: 'images/magician.png'
+    },
+    {
+      name: 'chariot',
+      img: 'images/chariot.png'
+    },
+    {
+      name: 'emperor',
+      img: 'images/emperor.png'
+    },
+    {
+      name: 'fool',
+      img: 'images/fool.png'
+    },
+    {
+      name: 'hierophant',
+      img: 'images/hierophant.png'
+    },
+    {
+      name: 'lovers',
+      img: 'images/lovers.png'
+    },
+    {
+      name: 'magician',
+      img: 'images/magician.png'
     }
-  
-    //check for matches
-    function checkForMatch() {
-      const cards = document.querySelectorAll('img')
-      const optionOneId = cardsChosenId[0]
-      const optionTwoId = cardsChosenId[1]
-      
-      if(optionOneId == optionTwoId) {
-        cards[optionOneId].setAttribute('src', 'assets/blank.png')
-        cards[optionTwoId].setAttribute('src', 'assets/blank.png')
-        alert('you did it!')
-      }
-      else if (cardsChosen[0] === cardsChosen[1]) {
-        alert('Great job')
-        cards[optionOneId].setAttribute('src', 'assets/win.png')
-        cards[optionTwoId].setAttribute('src', 'assets/win.png')
-        cards[optionOneId].removeEventListener('click', flipCard)
-        cards[optionTwoId].removeEventListener('click', flipCard)
-        cardsWon.push(cardsChosen)
-      } else {
-        cards[optionOneId].setAttribute('src', 'assets/blank.png')
-        cards[optionTwoId].setAttribute('src', 'assets/blank.png')
-        alert('nope.')
-      }
-      cardsChosen = []
-      cardsChosenId = []
-      resultDisplay.textContent = cardsWon.length
-      if  (cardsWon.length === cardArray.length/2) {
-        resultDisplay.textContent = 'Congratulations!'
-      }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  let cardsChosen = []
+  let cardsChosenId = []
+  let cardsWon = []
+
+  //create board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
     }
-  
-    //flip card
-    function flipCard() {
-      let cardId = this.getAttribute('data-id')
-      cardsChosen.push(cardArray[cardId].name)
-      cardsChosenId.push(cardId)
-      this.setAttribute('src', cardArray[cardId].img)
-      if (cardsChosen.length ===2) {
-        setTimeout(checkForMatch, 500)
-      }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Its a match')
     }
-  
-    createBoard()
-  })
-  
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'images/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations, The cards have been found!'
+    }
+  }
+
+  //flip card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length ===2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  createBoard()
+})
